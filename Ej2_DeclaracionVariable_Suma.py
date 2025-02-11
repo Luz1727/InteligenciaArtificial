ebdf51ef6f34ba85687656dd281622107a331e5c
LuzArlethLB <90057862+Luz1727@users.noreply.github.com>
2025-02-11 08:09:10 -0600

    Subiendo el archivo Python

---
+++ Ej2_DeclaracionVariable_Suma.py
@@ -0,0 +1,13 @@
+#Declarar variables
+nombre= "Juan"
+edad = 25
+altura = 1.75
+
+#Imprimir información
+print("nombre: ", nombre)
+print("Edad: ", edad)
+print("Altura: ", altura)
+
+#Operacion Simple
+edad_en_5_anos= edad + 5
+print("Dentro de 5 años tendre " , edad_en_5_anos, "años")
