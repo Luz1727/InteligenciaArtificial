bdb9fdaeb0885a03bfcf5c786879bbe404e1b16f
LuzArlethLB <90057862+Luz1727@users.noreply.github.com>
2025-02-11 08:17:45 -0600

    Subiendo el archivo Python

---
+++ Ej4_EntradaDatos.py
@@ -0,0 +1,6 @@
+nombre = input("Cual es tu nombre? ")
+edad= int(input("¿Cuantos años tienes? "))
+altura = float(input("¿Cuanto mides? (en metros) "))
+
+#Mostrar los datos introducidos
+print("Hola ", nombre, " Tienes ", edad, " años y mides ", altura, " metros ")
