58260cd2483ea5fde80899ef8aaa4e5a04fdac11
LuzArlethLB <90057862+Luz1727@users.noreply.github.com>
2025-02-11 07:58:40 -0600

    primer commit

---
+++ Ej1DeclaracionVariable.py
@@ -0,0 +1,10 @@
+#Declarar variables
+nombre= "Juan"
+edad = 25
+altura = 1.75
+
+#Imprimir información
+print("nombre: ", nombre)
+print("Edad: ", edad)
+print("Altura: ", altura)
+print("En 5 años tendré 30 años ")
